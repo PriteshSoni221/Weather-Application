7823c44a52e99b82427010a732fecac8b719e6a9
Pritesh Soni <55101088+PriteshSoni221@users.noreply.github.com>
2020-05-28 00:22:53 +0530

    API is added in the GUI app. Modify the result and print it on the label

--- weatherApp.py
+++ weatherApp.py
@@ -1,8 +1,25 @@
 import tkinter as tk
+import requests
 
 # variables
-var_width = 640
-var_height = 480
+var_width = 600
+var_height = 600
+
+
+def test_function(entry):
+    print("Entered city is :",entry)
+
+
+# API key : 2f1fa49a746c6674e86f07a6caddd95b
+# API call : api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={your api key}
+def get_weather(city):
+    api_key = "2f1fa49a746c6674e86f07a6caddd95b"
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    parameters = {"APPID": api_key, "q": city, "units": "metric"}
+    response = requests.get(url, params=parameters)
+    print(response.json())
+
+
 
 # root is the lowest level of our window. canvas and frame stay on top of root.
 root = tk.Tk()
@@ -18,26 +35,27 @@ background_label.place(x=0, y=0, relheight=1, relwidth=1)
 # """we are using place method with relative height and width method. So, if the size of window will change, size of our
 #     frame will change, too. place method is a nice way to organise widgets on the frame because it is very responsive
 #     frame will contain all the other widgets like buttons, labels, etc. We could have multiple frameworks. """
-upper_frame = tk.Frame(root, bg="#1ddbf0")
+upper_frame = tk.Frame(root, bg="#82e1f5")
 upper_frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
 
-# """pack method is to join the widget with other widget(frame, canvas, etc). We are saving one line of code by using pack
-#     method right after we are initializing it. pack method has limited flexibility. On the other hand, grid method has
-#     quite good flexibility and place is just the best which I have used for frame.
+# """pack method is to join the widget with other widget(frame, canvas, etc). We are saving one line of code by using
+#     pack method right after we are initializing it. pack method has limited flexibility. On the other hand, grid
+#     method has quite good flexibility and place is just the best which I have used for frame.
 #     button = tk.Button(frame, text="text button", fg="#850411", bd=4).pack()
 #     label = tk.Label(frame, text="this is a Label", bd=4).pack(side="right")
 #     entry = tk.Entry(frame, cursor="xterm").pack(side="right") """
 
-entry = tk.Entry(upper_frame, cursor="xterm", bd=3)
-entry.place(relwidth=0.7, relheight=1)
+text_box = tk.Entry(upper_frame, cursor="xterm", bd=3)
+text_box.place(relwidth=0.7, relheight=1)
 
-button = tk.Button(upper_frame, text="Weather Info", fg="#850411", bd=4)
-button.place(relx = 0.75, relheight = 1, relwidth = 0.25)
+weather_info_button = tk.Button(upper_frame, text="Weather Info", fg="#850411", font=40,
+                                command=lambda: get_weather(text_box.get()))
+weather_info_button.place(relx=0.75, relheight=1, relwidth=0.25)
 
-lower_frame = tk.Frame(root, bg="#1ddbf0")
+lower_frame = tk.Frame(root, bg="#82e1f5")
 lower_frame.place(relx=0.1, rely=0.3, relwidth = 0.8,relheight=0.6)
-label = tk.Label(lower_frame, text="Weather details will appear here", bd=5, justify="center")
-label.place(relx = 0.05, rely=0.05, relheight = 0.90, relwidth=0.90)
+label = tk.Label(lower_frame, text="Weather details will appear here", bd=10, justify="center")
+label.place(relx=0.02, rely=0.02, relheight = 0.96, relwidth=0.96)
 
 
 root.mainloop()
